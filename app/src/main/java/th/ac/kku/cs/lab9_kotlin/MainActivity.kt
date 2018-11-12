a8611ff3244da772d0fafc00b3add84d2385653a
supawadee2541 <44769899+supawadee2541@users.noreply.github.com>
2018-11-12 17:02:15 +0700

    lab9

---
+++ app/src/main/java/th/ac/kku/cs/lab9_kotlin/MainActivity.kt
@@ -0,0 +1,40 @@
+package th.ac.kku.cs.lab9_kotlin
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import th.ac.kku.cs.lab9_kotlin.R.id.button_send
+
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val KEY_DATA_STUDENT = "keyDataStudent"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        button_send.setOnClickListener {
+            val Intent = Intent( this,SecondActivity::class.java)
+
+            val dataStudent = Student(
+                 edit_text_id_std.text.toString(),
+                    edit_text_first_name.text.toString(),
+                    edit_text_last_name.text.toString()
+            )
+            intent.putExtra(KEY_DATA_STUDENT,dataStudent)
+            startActivity(intent)
+        }
+
+
+
+
+        }
+
+
+}
+
